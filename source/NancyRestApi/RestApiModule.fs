2c33d77a0f01e3ae905992565967238bb2c69e0e
Omar Diego Vera Ustariz <diego.ustariz@check24.de>
2015-03-23 18:36:34 +0100

    Initial version of the rest api

---
+++ source/NancyRestApi/RestApiModule.fs
@@ -0,0 +1,19 @@
+﻿module RestApiModule
+
+open Nancy
+open Nancy.ModelBinding
+
+type ServiceInfo = {Name: string; Version : string; GitSha: string; ReleaseNotes: string} 
+
+type ServiceInfoRequest = {Name: string; Environment: string}
+
+type RestApiModule() as this =
+    inherit NancyModule()
+    do
+        this.Get.["/serviceInfo/{serviceName}/{environment}/"] <- fun parameters ->
+                    let serviceName = (parameters :?> Nancy.DynamicDictionary).["serviceName"]
+                    let environment = (parameters :?> Nancy.DynamicDictionary).["environment"]
+                    printfn "%O" serviceName
+                    let response = {Name= "service1"; Version="version1"; GitSha="weasdfsd"; ReleaseNotes="notes"}
+                    this.Response.AsJson(response) :> obj
+
---
+++ source/NancyRestApiRunner/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+open Nancy
+
+[<EntryPoint>]
+let main argv = 
+    let nancy = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:" + "8100"))
+    nancy.Start()
+    while true do Console.ReadLine() |> ignore
+    0
